0e36e1ecb11171e7eaf77d151bdb970ca495e232
郑智文 <diamondbackwise@outlook.com>
2026-09-09 09:11:02 +0800

    feat(chunks): 添加分块策略枚举定义
    - 新增 ChunkingStrategyEnum 枚举类
    - 定义定长切分策略 (fixed_length)
    - 定义语义切分策略 (semantic)
    - 添加枚举类文档注释
    - 包含文件头版权信息

---
+++ app/libs/enums.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+文件名：enums.py
+文件描述: 
+作者: 郑智文
+创建日期: 2026/9/9 09:07
+版本: 1.0
+IDE: PyCharm
+
+Copyright (c) 2026 星际区块链（深圳）有限公司. All rights reserved.
+"""
+from enum import Enum
+
+
+class ChunkingStrategyEnum(Enum):
+    """
+    分块策略枚举
+
+    系统中应该统一存放策略枚举，这里为了
+    """
+    定长切分 = 'fixed_length'
+    语义切分 = 'semantic'
